5b777da73445b364069ab70150e9458d3bf4b8ea
Aryan ved <yadhuvanshiaryan@gmail.com>
2024-11-15 20:19:45 +0530

    Your commit message here

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Payments from "./components/Payments";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900">
+    <div className="overflow-hidden text-neutral-300 antialiased selection:bg-cyan-500 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
       </div>
